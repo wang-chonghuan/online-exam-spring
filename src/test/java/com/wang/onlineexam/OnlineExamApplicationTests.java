c40f6163c9ecd007fa057c9cfa57d9e973d53dcd
wang-chonghuan <wangchonghuan@gmail.com>
2022-10-28 11:51:35 +0100

    spring initializr files

---
+++ src/test/java/com/wang/onlineexam/OnlineExamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wang.onlineexam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineExamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
