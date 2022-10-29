fc59db6f3b11d073c8272e6df47564d317208dfc
wang-chonghuan <wangchonghuan@gmail.com>
2022-10-29 23:13:46 +0100

    add 2 unit tests for repository and MVC-REST. add a controller Teacher.

---
+++ src/test/java/com/wang/onlineexam/TeacherRestTest.java
@@ -0,0 +1,36 @@
+package com.wang.onlineexam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpHeaders;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class TeacherRestTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void testPostTeacher() throws Exception {
+        ResultActions r = this.mockMvc.perform(post("/create-teacher")
+                .content("{\"name\":\"test.teacher1.name\", \"email\":\"test.teacher1.email\"}")
+                .header(HttpHeaders.CONTENT_TYPE, "application/json"));
+        r = r.andDo(print()).andExpect(status().isCreated());
+    }
+
+    @Test
+    public void testDataApiPostTeacherDrapi() throws Exception {
+        ResultActions r = this.mockMvc.perform(post("/data-api/teachers")
+                .content("{\"name\":\"test.teacher2.name\", \"email\":\"test.teacher2.email\"}")
+                .header(HttpHeaders.CONTENT_TYPE, "application/json"));
+        r = r.andDo(print()).andExpect(status().isCreated());
+    }
+}
